69f60241bd018fd8cac7773a191978af7a79bbad
im-coder-lg <imcoderlg@protonmail.com>
2021-11-29 06:31:45 +0000

    Hi Git!

--- install-node.sh
+++ install-node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install curl software-properties-common
+sudo apt-get install -y curl software-properties-common
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt install nodejs
 node -v && npm -v
